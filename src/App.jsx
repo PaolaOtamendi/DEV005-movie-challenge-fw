2aac078dfe4c7bab1d428efa4586d41c7ed034ec
PaolaOtamendi <paola_otamendi23@hotmail.com>
2023-09-04 09:35:27 -0600

    fix cambio de funciones a documento js

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,61 @@
 import { useEffect, useState } from 'react'
 import './App.css'
-/* const fetch = require('node-fetch'); */
+import { obtenerPeliculas } from './Servicios/peticiones';
 
 
 function App() {
 
+  // Estado para almacenar las películas obtenidas de la API
   const [peliculas, setPeliculas] = useState([]);
 
+/*   // Función para obtener las películas de la API
   const obtenerPeliculas = async () => {
-    const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=true&include_video=true&language=en-US&page=1&sort_by=popularity.desc';
+    const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
+    
+    // Opciones para la solicitud HTTP
     const options = {
-      method: 'GET',
+      method: 'GET', // Metodo (Verbo) HTTP
       headers: {
         accept: 'application/json',
         Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMzRhNGY5ZjgzMTQ0MzZjNmJjZDA5N2YxZmM4OTAyMyIsInN1YiI6IjY0ZWUyY2EwZTBjYTdmMDBjYmUzNzFiMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.9qxsVsWeO3vPFHiRZYW2319NaMtkdEfL0MqrjzuCDd8'
       }
     };
 
-    try {
-      const response = await fetch(url, options);
-      const data = await response.json();
-      console.log(data, 20); // Muestra los datos en la consola de la busqueda realizada
-      setPeliculas(data.results); // Establece los datos en el estado peliculas
-    } catch (error) {
-      console.error('Error:', error);
+    try { // Promesa
+      // Realiza una solicitud HTTP GET a la URL de la API con las opciones especificadas
+      const response = await fetch(url, options); //fetch consume la api con la url y las opciones
+      const data = await response.json();         // Convierte la respuesta en un objeto JSON
+      console.log(data, 20);                      // Muestra los datos en la consola de la busqueda realizada con la respuesta en json
+      setPeliculas(data.results);                 // Establece el estado "peliculas" con los resultados obtenidos de la API
+    } catch (error) {                             // Devuelte la promesa error en caso de no cumplirse la solicitud
+      console.error('Error:', error); 
     }
   }
-
-  // Llama a obtenerPeliculas cuando el componente se monta
+ */
+  
+  // Efecto que se ejecuta cuando el componente se monta (una sola vez)
   useEffect(() => {
-    obtenerPeliculas();
+    obtenerPeliculas() // Llama la funcion obtenerPeliculas cuando el componente se monta
+    .then(results => setPeliculas(results))
+    .catch(error => console.error('Error', error));
   }, []);
 
   
   return (
-    <>
+    
     <div className='parrafo1'>
     {
-      peliculas.map(pelicula => 
-        <p key={pelicula.id} className='parrafo'>
-          {pelicula.title}</p>
-      )
+      peliculas.map(pelicula => (
+        <div key={pelicula.id}>
+          <img src={`http://image.tmdb.org/t/p/w300/${pelicula.poster_path}`} alt={''} />
+          <p className='parrafo'>
+          {pelicula.title}
+          </p>
+        </div>  
+      ))
     }
     </div>
-    </>
+    
   )
 }
 
--- src/Servicios/peticiones.js
+++ src/Servicios/peticiones.js
@@ -1,32 +1,24 @@
-/* import axios from 'axios';
-
-const apiKey = '134a4f9f8314436c6bcd097f1fc89023';
-const apiUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`;
-/* const apiUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`; */
-
-
-/* axios.get(apiUrl)
-  .then(response => {
-    // Manipula los datos de respuesta aquí
-    console.log(response.data);
-  })
-  .catch(error => {
-    // Maneja los errores aquí
-    console.error('Error:', error);
-  });  */
-
-  const fetch = require('node-fetch');
-
-const url = 'https://api.themoviedb.org/3/movie/movie_id/videos?language=en-US';
-const options = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMzRhNGY5ZjgzMTQ0MzZjNmJjZDA5N2YxZmM4OTAyMyIsInN1YiI6IjY0ZWUyY2EwZTBjYTdmMDBjYmUzNzFiMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.9qxsVsWeO3vPFHiRZYW2319NaMtkdEfL0MqrjzuCDd8'
-  }
+// Función para obtener las películas de la API
+export const obtenerPeliculas = async () => {
+    const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
+        
+    // Opciones para la solicitud HTTP
+    const options = {
+        method: 'GET', // Metodo (Verbo) HTTP
+        headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMzRhNGY5ZjgzMTQ0MzZjNmJjZDA5N2YxZmM4OTAyMyIsInN1YiI6IjY0ZWUyY2EwZTBjYTdmMDBjYmUzNzFiMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.9qxsVsWeO3vPFHiRZYW2319NaMtkdEfL0MqrjzuCDd8'
+        }
+    };
+    
+    try { // Promesa
+        // Realiza una solicitud HTTP GET a la URL de la API con las opciones especificadas
+        const response = await fetch(url, options); //fetch consume la api con la url y las opciones
+        const data = await response.json();         // Convierte la respuesta en un objeto JSON
+        console.log(data, 20);                      // Muestra los datos en la consola de la busqueda realizada con la respuesta en json
+        //setPeliculas(data.results);               // Establece el estado "peliculas" con los resultados obtenidos de la API
+        return data.results;
+    } catch (error) {                               // Devuelte la promesa error en caso de no cumplirse la solicitud
+        console.error('Error:', error); 
+    }
 };
-
-fetch(url, options)
-  .then(res => res.json())
-  .then(json => console.log(json))
-  .catch(err => console.error('error:' + err));
