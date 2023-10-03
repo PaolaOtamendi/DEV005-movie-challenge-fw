522543202877c77b4af595d19dbcd1d3bbf675fd
PaolaOtamendi <paola_otamendi23@hotmail.com>
2023-10-03 16:42:22 -0600

    fix componente header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,20 +1,25 @@
 import { Link } from "react-router-dom";
 import "./header.css"
+import logo from "../../assets/logo.png";
 
 const Header = () => {
     return(
         <div className="containerheader">
+            <div className="containerlogo">
             <div className="logo">
-                <img src={""} alt={""}></img>
+                <img src={logo} alt="" style={{ width: "68px", height: "auto" }}></img>
             </div>
             <div className="nameHeader">
                 <p>CINEMA PLANET</p>
             </div>
+            </div>
+            {/* <div className="containerlink"> */}
             <div className="containerNavbar">
                 <Link to='/Home' className="home">Home</Link>
                 <Link to='/Populares' className="populares">Populares</Link>
                 <Link to='/Estrenos' className="estrenos">Estrenos</Link>
             </div>
+            {/* </div> */}
         </div>
     );
 }
